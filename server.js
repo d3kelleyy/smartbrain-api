1c6ee663013f1114e8934520f7f3361dbd095b34
unknown <d3kelley@gmail.com>
2020-10-14 21:35:29 -0400

    ignore node modules

--- server.js
+++ server.js
@@ -2,6 +2,21 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const bcrypt = require("bcrypt");
 const cors = require("cors");
+const knex = require('knex')
+
+const db = knex({
+  client: 'pg',
+  connection: {
+    host : '127.0.0.1',
+    user : 'postgres',
+    password : 'redsox86',
+    database : 'smartbrain'
+  }
+});
+
+db.select('*').from('users').then(data => {
+  console.log(data)
+});
 
 const app = express();
 
@@ -36,20 +51,6 @@ app.get("/", (req, res) => {
 });
 
 app.post("/signin", (req, res) => {
-  bcrypt.compare(
-    "apples",
-    "$2b$10$pFB1.nLhXk7IGuTi89tfCukJozdT6MA777EwleLjFFFro5CKBVOU.",
-    function (err, result) {
-      console.log("first guess", result);
-    }
-  );
-  bcrypt.compare(
-    "bananas",
-    "$2b$10$MlHwcb87/vIum5gSe9/TRe/kGiQ3bpS7LsesJqD0A0bxMD8jXchGW",
-    function (err, result) {
-      console.log("second guess", result);
-    }
-  );
   if (
     req.body.email === database.users[0].email &&
     req.body.password === database.users[0].password
@@ -62,31 +63,29 @@ app.post("/signin", (req, res) => {
 
 app.post("/register", (req, res) => {
   const { email, name, password } = req.body;
-  bcrypt.hash(password, saltRounds, function (err, hash) {
-    console.log(hash);
-  });
-  database.users.push({
-    id: "125",
+  db('users')
+  .returning('*')
+  .insert({
     name: name,
     email: email,
-    entries: 0,
-    joined: new Date(),
-  });
-  res.json(database.users[database.users.length - 1]);
+    joined: new Date()
+  }).then(user => {
+    res.json(user[0]);
+  })
+  .catch(err => res.status(400).json('unable to register'))
 });
 
 app.get("/profile/:id", (req, res) => {
   const { id } = req.params;
-  let found = false;
-  database.users.forEach((user) => {
-    if (user.id === id) {
-      found = true;
-      return res.json(user);
+  db.select('*').from('users').where({id})
+  .then(user => {
+    if (user.length) {
+      res.json(user[0]);
+    } else {
+      res.status(400).json('Not Found')
     }
-  });
-  if (!found) {
-    res.status(400).json("user not found");
-  }
+  })
+  .catch(err => res.status(400).json('error getting user'));
 });
 
 app.put("/image", (req, res) => {
@@ -104,6 +103,6 @@ app.put("/image", (req, res) => {
   }
 });
 
-app.listen(3001, () => {
-  console.log("app is running on port 3001");
+app.listen(3000, () => {
+  console.log("app is running on port 3000");
 });
